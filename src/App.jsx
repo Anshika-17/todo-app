077afcf017146e365078b94fcee1fbc7f9d90844
Akshay <5638.akshay@gmail.com>
2024-02-14 23:18:57 +0530

    Layout made

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,52 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const handleEdit = () => {
+
+  }
+  const handleDelete = () => {
+
+  }
+
+  const handleAdd = () => {
+    setTodos([...todos, {todo, iscompleted: false}])
+    setTodo("")
+    console.log(todos)
+    
+  }
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+
+  
 
   return (
     <>
       <Navbar />
-      <div className="conatainer mx-auto">
-        <div className="bg-red-600 ">
-          <h1>Your Todos</h1>
-        </div>
+      <div className=" container my-5 rounded-xl p-5 bg-violet-100 mx-auto min-h-[80vh]">
+          <div className="addTodo my-5">
+            <h2 className="text-lg font-bold">Add a Todo</h2>
+            <input onChange={handleChange} value={todo} type="text" className="w-1/2"/>
+            <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 p-2 font-bold text-sm py-1 text-white rounded-md mx-6 ">Add</button>
+          </div>
+          <h2 className="text-lg font-bold">Your Todos</h2>
+          <div className="todos">
+            {todos.map(item => {
+
+            return <div key={todo} className="todo flex w-1/2 my-3 justify-between">
+              <input type="checkbox" value={todo.iscompleted} name="" id="" />
+              <div className={item.iscompleted?"line-through":""}>{item.todo}</div>
+              <div className="buttons">
+                <button onClick={handleEdit} className="bg-violet-800 hover:bg-violet-950 p-2 font-bold text-sm py-1 text-white rounded-md mx-1">Edit</button>
+                <button onClick={handleDelete} className="bg-violet-800 hover:bg-violet-950 p-2 font-bold text-sm py-1 text-white rounded-md mx-1">Delete</button>
+              </div>
+            </div>
+            })}
+          </div>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-slate-700 text-white'>
+    <nav className='flex justify-between bg-violet-900  text-white py-2'>
         <div className="logo">
-            <span className='font-bold text-xl mx-8'>Todo</span>
+            <span className='font-bold text-xl mx-8 '>Todo</span>
         </div>
         <ul className="flex gap-8 mx-8">
-            <li className='cursor-pointer'>Home</li>
-            <li className='cursor-pointer'>Your Task</li>
+            <li className='cursor-pointer hover:font-bold transition-all '>Home</li>
+            <li className='cursor-pointer hover:font-bold transition-all '>Your Task</li>
         </ul>
     </nav>
   
